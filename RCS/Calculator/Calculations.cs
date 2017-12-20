5fccfedbbf67ec0e473b16cd375de7c3db343224
armandsbrigis <armandsbrigis@gmail.com>
2017-12-20 19:59:41 +0200

    new programm - calculator

---
+++ RCS/Calculator/Calculations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculations
+    {
+        public int AskUserForNumber()
+        {
+            //izvadīt tekstu, kas paprasa ciparu
+            Console.WriteLine("please enter number");
+            //ielasīt ciparu no konsoles
+            //izveido mainigo kas glaba tekstu
+            int number;
+            string inputText = Console.ReadLine();
+            //pārveido lielotāja tekstu par ciparu
+            bool success = Int32.TryParse(inputText, out number);
+
+            //int 32 ir klases ar funkcijām
+
+            if (success == false)
+            {
+                Console.WriteLine("Sorry, you are stupid");
+                number = AskUserForNumber();
+
+            }
+
+            return number;
+        }
+
+    }
+
+}
---
+++ RCS/Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //izveidot kalkulēšanas objektu
+            Calculations calc;
+            calc = new Calculations();
+
+            //paprasīt lietotājam vērtību
+            int firstNumber = calc.AskUserForNumber();
+            // paprasīt otru vērtību
+            int secondNumber = calc.AskUserForNumber();
+            // saskaitīt 
+            int result = firstNumber + secondNumber;
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
